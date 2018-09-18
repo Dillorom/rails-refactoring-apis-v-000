ba133d4f236af166cb63cb6dba91c282eef92d44
Dillorom <dabdulloeva@gmail.com>
2018-09-18 10:56:44 -0700

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180917195809) do
+
+  create_table "github_repos", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
